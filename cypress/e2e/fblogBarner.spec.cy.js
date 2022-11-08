3cdbb01457d4b53f078fb6a88a3aad502cd93186
EBENEZER ASHONG <ebenezer.ashong@amalitech.org>
2022-11-08 17:50:40 +0000

    Added other tests

--- cypress/e2e/fblogBarner.spec.cy.js
+++ cypress/e2e/fblogBarner.spec.cy.js
@@ -1,4 +1,5 @@
 describe('explores the frewelance blog page', () => {
+
   it('opens the blog page', () => {
     cy.visit('localhost:3000')
     cy.get('[data-cy="thefairwork_logo"]').should('be.visible')
@@ -6,9 +7,7 @@ describe('explores the frewelance blog page', () => {
     cy.get('.Banner_description__Fd_42')
     .contains('Explore new technologies, industry insights, stories of success, tools, and inspiration about the future of tech talent and the new world of work')
     cy.get("input[placeholder='What do you want to read about?']").type('Hello Freelancer')
-    
-  
-  })
+     })
 
   it('has blog articles containing images of the writer', () => {
     //cy.visit('localhost:3000')
@@ -23,4 +22,41 @@ describe('explores the frewelance blog page', () => {
     cy.get("[data-cy='styles.avatarprofilePicture']").should('be.visible')
     cy.get('.container > .MuiGrid-container > :nth-child(1) > #cardMain > .MuiCardContent-root > .MuiButtonBase-root > .MuiButton-label').should('have.text', 'READ MORE').should('be.visible').click()
   })
+
+  it("has pagination including Previous and next", () => {
+    //cy.visit('localhost:3000')
+    cy.get(':nth-child(1) > [data-cy="paging pre-next"]').should('be.visible').click()
+    cy.get(':nth-child(2) > [data-cy="paging p"]').should('be.visible').click()
+    cy.get('.Pagination_pagination__ShHE2 > :nth-child(3) > p').should('be.visible').click()
+    cy.get(':nth-child(8) > [data-cy="paging p"]').should('be.visible').click()
+    cy.get(':nth-child(9) > [data-cy="paging pre-next"]').should('be.visible').click() 
+})
+
+it('has most read articles section', () => {
+    cy.visit('localhost:3000')
+    cy.get('[data-cy="most r"]').should('have.text', 'Most Read Articles')
+ 
+})
+ 
+it('displays various links in the footer', () => {
+    cy.get('[data-cy="forClients"]').should('have.text', 'For Clients')
+    cy.get('[data-cy="BecomeaClients"]').should('have.text', 'Become A Client')
+    cy.get('[data-cy="forFreelancers"]').should('have.text', 'For Talent')
+    cy.get('[data-cy="BecomeaFairworkerLink"]').should('have.text', 'Become A FairWorker')
+    cy.get('[data-cy="AmalitechBigiskillT"]').should('have.text', 'AmaliTech Digital Skills Training')
+    cy.get('[data-cy="AmalitechCompany"]')
+    cy.get('[data-cy="AboutTheFairWork"]')
+    cy.get('[data-cy="AmalitechTeam"]')
+    cy.get('[data-cy="AmalitechServices"]')
+    cy.get('[data-cy="PrivacyPolicy"] > p')
+    cy.get('[data-cy="ContactUs"]')
+
+})
+it('has social media links like Facebook and Linkedin', () => {
+  cy.get('[data-cy="followUs"]').should('have.text', 'Follow Us:')
+  cy.get('[ data-cy="LinkedIn"]')//.click()
+  cy.get('[ data-cy="Facebook"]')//.click()
+ //cy.get('[data-cy="copyRightMsg"]', {timeout:10000})
+ // .should('include', ' &copy; Copyright {new Date().getFullYear()} TheFairWork. All Rights Reserved.')
+})
 })
